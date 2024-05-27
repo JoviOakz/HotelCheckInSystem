6e18b93817c075123c736739db812511b305b12d
JoviOakz <joao.mateussdc@gmail.com>
2024-05-27 15:59:09 -0300

    register controller

--- src/main/java/com/jovioakz/LoginSceneController.java
+++ src/main/java/com/jovioakz/LoginSceneController.java
@@ -16,13 +16,12 @@ import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
 public class LoginSceneController {
-    
+
     // Vamos fazer uma função CreateScene que irá
     // criar a cena apartir de um FXMLLoader carregando
     // o .fxml.
     public static Scene CreateScene() throws Exception {
-        URL sceneUrl = LoginSceneController.class
-                .getResource("login-scene.fxml");
+        URL sceneUrl = LoginSceneController.class.getResource("login-scene.fxml");
         Parent root = FXMLLoader.load(sceneUrl);
         Scene scene = new Scene(root);
         return scene;
@@ -33,38 +32,34 @@ public class LoginSceneController {
     // que aparece aqui.
     @FXML
     protected Button btLogin;
+
     @FXML
     protected TextField tfLogin;
+
     @FXML
     protected PasswordField pfPass;
+    
     @FXML
     protected CheckBox cbPass;
 
     // Evento submit executado ao rodar a aplicação.
     @FXML
     protected void submit(ActionEvent e) throws Exception {
-        Authentification auth = Authentification.tryLogin(
-                tfLogin.getText(), pfPass.getText());
+        Authentification auth = Authentification.tryLogin(tfLogin.getText(), pfPass.getText());
+
         if (!auth.userExists()) {
-            Alert alert = new Alert(
-                    AlertType.ERROR,
-                    "Usuário inexistente.",
-                    ButtonType.OK);
+            Alert alert = new Alert(AlertType.ERROR, "Usuário inexistente.", ButtonType.OK);
             alert.showAndWait();
             return;
         }
         if (auth.getUser() == null) {
-            Alert alert = new Alert(
-                    AlertType.ERROR,
-                    "Senha incorreta.",
-                    ButtonType.OK);
+            Alert alert = new Alert(AlertType.ERROR, "Senha incorreta.", ButtonType.OK);
             alert.showAndWait();
             return;
         }
-        
+
         // Fechando o login
-        Stage crrStage = (Stage) btLogin
-                .getScene().getWindow();
+        Stage crrStage = (Stage) btLogin.getScene().getWindow();
         crrStage.close();
 
         // // Abrindo a tela principal
--- src/main/java/com/jovioakz/RegisterPageController.java
+++ src/main/java/com/jovioakz/RegisterPageController.java
@@ -12,8 +12,10 @@ import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.CheckBox;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class RegisterPageController {
     
@@ -21,10 +23,40 @@ public class RegisterPageController {
     // criar a cena apartir de um FXMLLoader carregando
     // o .fxml.
     public static Scene CreateScene() throws Exception {
-        URL sceneUrl = RegisterPageController.class
-                .getResource("register-page.fxml");
+        URL sceneUrl = RegisterPageController.class.getResource("register-page.fxml");
         Parent root = FXMLLoader.load(sceneUrl);
         Scene scene = new Scene(root);
         return scene;
+    }
+
+    // Variáveis que representam os componentes
+    // Note que id/field devem ser iguais ao nome
+    // que aparece aqui.
+    @FXML
+    protected Button btConfirm;
+
+    @FXML
+    protected TextField tfName;
+
+    @FXML
+    protected DatePicker dpStart;
+
+    @FXML
+    protected DatePicker dpEnd;
+
+    @FXML
+    protected TextField tfCheckIn;
+
+    @FXML
+    protected TextField tfCheckOut;
+
+    @FXML
+    protected void submit(ActionEvent e) throws Exception {
+        
+        
+
+        // Fechando o register
+        Stage crrStage = (Stage) btConfirm.getScene().getWindow();
+        crrStage.close();
     }
 }
