bfbb5bbca2938ba7a94fc372bc96cd2b290a0544
akda007 <akdagmr007s@gmail.com>
2024-05-27 15:44:32 -0300

    Created Part of the GUI

---
+++ src/main/java/com/jovioakz/MainSceneController.java
@@ -0,0 +1,30 @@
+package com.jovioakz;
+
+import java.net.URL;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+// import javafx.stage.Stage;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class MainSceneController {
+    
+    // Vamos fazer uma função CreateScene que irá
+    // criar a cena apartir de um FXMLLoader carregando
+    // o .fxml.
+    public static Scene CreateScene() throws Exception {
+        URL sceneUrl = MainSceneController.class
+                .getResource("main-page.fxml");
+        Parent root = FXMLLoader.load(sceneUrl);
+        Scene scene = new Scene(root);
+        return scene;
+    }
+}
